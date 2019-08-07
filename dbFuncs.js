31e61eeef6e3800e5c2f5333c737efd2c2fae797
Ofir Levi <ofirl24@gmail.com>
2019-08-07 17:07:15 +0300

    test refactor

---
+++ dbFuncs.js
@@ -0,0 +1,32 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: false
+});
+
+async function executeQuery(query) {
+    try {
+        const client = await pool.connect()
+        const result = await client.query(query);
+        const results = { 'results': (result) ? result.rows : null };
+        client.release();
+        return results;
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+}
+
+function getAlllTickets(req, res) {
+    return res.json(executeQuery('SELECT * FROM tickets'));
+}
+
+function getTickets(req, res) {
+    console.log(req);
+    return res.json(executeQuery('SELECT * FROM tickets'));
+}
+
+module.exports = {
+    getAlllTickets,
+    getTickets
+}
--- index.js
+++ index.js
@@ -2,33 +2,31 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: false
-});
-
+const dbFuncs = require('./dbFuncs');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/about', (req, res) => res.json({hello :'world'}))
-  .get('/getalltickets', (req, res) => res.json([{hello :'world'}, {id : 2}]))
+  .get('/about', (req, res) => res.json({ hello: 'world' }))
+
+  .get('/getalltickets', (req, res) => dbFuncs.getAlllTickets(req, res))
+  .get('/gettickets', (req, res) => dbFuncs.getTickets(req, res))
+
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
       console.log(client);
       const result = await client.query('SELECT * FROM tickets');
       console.log(result);
-      const results = { 'results': (result) ? result.rows : null};
+      const results = { 'results': (result) ? result.rows : null };
       console.log(results);
-      res.render('pages/db', results );
+      res.render('pages/db', results);
       client.release();
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
     }
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
