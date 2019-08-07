358b28089ffe7d248606911de07c660ed050b832
Ofir Levi <ofirl24@gmail.com>
2019-08-07 16:39:46 +0300

    testing db

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: false
+});
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -9,4 +16,16 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/about', (req, res) => res.json({hello :'world'}))
   .get('/getalltickets', (req, res) => res.json([{hello :'world'}, {id : 2}]))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM tickets');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
